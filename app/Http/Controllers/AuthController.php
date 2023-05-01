84134dfc7a9306a1a73362bd5e83ba0e33f28038
Sergi Khizanishvili <sergi@Sergis-MacBook.local>
2023-05-01 22:22:15 +0400

    chore: removed unused code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginUserRequest;
 use App\Models\User;
 use App\Traits\HttpResponses;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
